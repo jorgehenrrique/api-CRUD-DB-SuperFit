e7d657213f12b47c6ba74703b335c73669a484ca
Geovana Framil <geovanaframil@hotmail.com>
2023-11-12 12:38:12 -0300

    Adicionado handle Report

---
+++ src/controllers/handleReport.ts
@@ -0,0 +1,21 @@
+import { json } from "express";
+import createDBClient from "../db/connection";
+import { ReportService } from "../services/report.service";
+
+export async function relatorio(req: any, res: any) {
+  const db = createDBClient();
+  await db.connect();
+  const reportService = new ReportService(db);
+
+  try {
+    const report = await reportService.get();
+    res.json(report);
+  } catch (error: any) {
+    res.status(500).json({
+      error: "Erro ao gerar relatório.",
+      message: error.message,
+    });
+  } finally {
+    await db.end();
+  }
+}
